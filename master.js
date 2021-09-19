c714a9b830faa2a878ae3f88d1eb1ac7c3dccf8a
Bruno Alécio <50933242+aleciobr@users.noreply.github.com>
2021-09-19 19:29:44 -0300

    img
    feito só a alteração de titulos

---
+++ master.js
@@ -0,0 +1,18 @@
+var listaGames = [
+    "https://cdn.staticneo.com/boxshots/MjAxNS8=/the-legend-of-zelda-breath-of-the-wild_frontcover_large_M0c2RbZqNN05L3E.jpg",
+    "https://www.mobygames.com/images/covers/l/56859-pokemon-blue-version-game-boy-front-cover.jpg",
+    "https://www.mobygames.com/images/covers/l/364131-killer-instinct-definitive-edition-xbox-one-front-cover.png",
+    "https://coverproject.sfo2.cdn.digitaloceanspaces.com/nintendo_64/n64_conkersbfd_au_label_thumb.jpg"];
+
+function Adicionar(){
+    var novoGame = document.getElementById("link").value;
+    listaGames.push(novoGame);
+
+}
+
+for(var i = 0; i < listaGames.length; i++){
+    document.write("<img src="+ listaGames[i] +">");
+}
+
+
+//https://e.snmc.io/lk/o/x/e04470ea5d9798cdf355151a0b023418/6877919
